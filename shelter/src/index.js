ddd33a662637724f44e7c3e8ca32b49a580ddaec
Mikhail <weedprotector@gmail.com>
2023-04-09 12:53:27 +0500

    feat: add aside bar

--- shelter/src/index.js
+++ shelter/src/index.js
@@ -1,1 +1,21 @@
-console.log(100)
+const burger = document.querySelector('.hamburger');
+const closeTriggers = document.querySelectorAll('[data-close]');
+const aside = document.querySelector('.aside');
+const substrate = document.querySelector('.substrate');
+
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('burger_flip');
+    aside.classList.toggle('show');
+    substrate.classList.toggle('substrate-show');
+    document.body.classList.toggle('scroll-lock');
+})
+
+closeTriggers.forEach(trigger => {
+    trigger.addEventListener('click', () => {
+        burger.classList.toggle('burger_flip');
+        aside.classList.toggle('show');
+        substrate.classList.toggle('substrate-show');
+        document.body.classList.toggle('scroll-lock');
+    })
+})
